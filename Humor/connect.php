13931688f004e51ac06d6b88f3ed43c002a48fbd
Hieu Do <hieu250192@gmail.com>
2021-02-22 08:47:55 +0700

    master Update code for saving image url

---
+++ Humor/connect.php
@@ -0,0 +1,21 @@
+<?php
+    $servername = "localhost";
+    $database = "app_giai_tri";
+    $username = "root";
+    $password = "";
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+
+    if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
